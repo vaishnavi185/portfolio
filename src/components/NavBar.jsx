4bfdc014c1dbbde6a972cef69de2a4232b6d8ddf
vaishnavi185 <vishu032004@gmail.com>
2024-06-05 15:20:07 +0530

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,34 +4,33 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
-import './nav.css';
+import '../components/Nav.css';
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home" className='nav-link'>Home</Nav.Link>
-          <Nav.Link href="#service" className='nav-link'>Services</Nav.Link>
-          <Nav.Link href="#portfolio" className='nav-link'>Portfolio</Nav.Link>
-          <Nav.Link href="#testimonials" className='nav-link'>Testimonials</Nav.Link>
-          <Nav.Link href="#blogs" className='nav-link'>Blogs</Nav.Link>
-          <Nav.Link href="#contact" className='nav-link'>Contact Us</Nav.Link>
+        <Nav.Link href="#home" className='nav-link' style={{ color: 'white', marginRight:"60px" }}>Home</Nav.Link>
+          <Nav.Link href="#service" className='nav-link'style={{ color: 'white', marginRight:"60px" }}>Services</Nav.Link>
+          <Nav.Link href="#portfolio" className='nav-link'style={{ color: 'white', marginRight:"60px" }}>Portfolio</Nav.Link>
+          <Nav.Link href="#testimonials" className='nav-link'style={{ color: 'white', marginRight:"60px" }}>Testimonials</Nav.Link>
+          <Nav.Link href="#blogs" className='nav-link'style={{ color: 'white' , marginRight:"60px"}}>Blogs</Nav.Link>
+          <Nav.Link href="#contact" className='nav-link'style={{ color: 'white', marginRight:"60px" }}>Contact Us</Nav.Link>
         </Nav>
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text className='icons'>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon" style={{ color: 'white' }}>
               <FontAwesomeIcon icon={faInstagram} />
             </a>
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon"style={{ color: 'white' }}>
               <FontAwesomeIcon icon={faFacebook} />
             </a>
-            <a href="https://x.com/vishu032004" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a href="https://x.com/vishu032004" target="_blank" rel="noopener noreferrer" className="social-icon"style={{ color: 'white' }}>
               <FontAwesomeIcon icon={faTwitter} />
             </a>
-            <a href="https://www.linkedin.com/in/vaishnavi-vaishnavi-686630250/" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a href="https://www.linkedin.com/in/vaishnavi-vaishnavi-686630250/" target="_blank" rel="noopener noreferrer" className="social-icon"style={{ color: 'white' }}>
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
           </Navbar.Text>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar></NavBar>
+   <div className='b'>
+   <Home></Home>
+   </div>
+  
+   
+   
   </React.StrictMode>
 );
 
