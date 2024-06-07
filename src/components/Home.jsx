50fd0b86f00253bc80a1c36fdc5b020559a27c16
vaishnavi185 <vishu032004@gmail.com>
2024-06-07 20:17:36 +0530

    services

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,24 +13,51 @@ export default function Home() {
     <NavBar></NavBar>
     <br></br>
     <br></br>
-<br></br><br></br>
+<br></br>
     <h1>Vaishnavi</h1>
 
 <br></br>
-<br></br>  
-<br></br>  
+  
+ 
      <Container className='content'>
      <Row>
         <Col>
         <Container className='services'>
           <h2>services</h2>
+          <br></br>
            <div className='app'>
             <img src='.\icon.png'className='ic1'></img>
             <p className='p1'>App development</p>
+            
            </div>
+           <p className='p2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo vel molestiae illum, 
+            </p>
+            <br></br>
+            <div className='app'>
+
+            <img src='.\icon2.png'className='ic1'></img>
+            <p className='p1'>web development</p>
+            
+           </div>
+           <p className='p2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo vel molestiae illum, 
+            </p>
+            <br></br>
+            <div className='app'>
+
+            <img src='.\icon3.png'className='ic1'></img>
+            <p className='p1'>  DataBase</p>
+          </div>
+           <p className='p2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo vel molestiae illum, 
+            </p>
+
         </Container>
         </Col>
-        <Col>2 of 3</Col>
+        <Col>
+        <div className='photo'>
+          <img src='.\Frame 481716.png' className='pic'></img>
+
+        </div>
+        </Col>
         <Col>3 of 3</Col>
       </Row>
      </Container>
