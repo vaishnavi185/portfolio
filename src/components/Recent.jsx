355e4eaa558d14025028fea944340a70222edac4
vaishnavi185 <vishu032004@gmail.com>
2024-06-11 11:29:42 +0530

    some

--- src/components/Recent.jsx
+++ src/components/Recent.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from 'react';
 import './rec.css';
+
 export default function Recent() {
   return (
     <div className='boxx'>
-        <br></br>
+      <img src='./image1.png' alt='Recent work'className='imo'></img>
+    
       <h3>Recent works</h3>
       <br></br>
       <div className='tabs'>
-    <div className='tab1'>
-      
-        <span>includes:</span>
-
-    </div>
+        <div className='tab1'>
+          <span>includes:</span>
+        </div>
+        <div className='tab'>
+          <h4>Website</h4>
+          <h4>APP</h4>
+        </div>
       </div>
+      <img src='./Image.png' alt='Recent work'className='im9'></img>
     </div>
-  )
+  );
 }
