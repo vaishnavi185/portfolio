860cd6dff2ae6aba31b1158a6038c39b58ddde1c
vaishnavi185 <vishu032004@gmail.com>
2024-06-18 21:30:06 +0530

    updated

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -18,24 +18,22 @@ export default function Service() {
    <br></br>     <div className='line'>
           <div className='Box'>
           <img src={bgImage} alt="img" className='im0' />
-          <h5 className='head'>Memory-Box</h5>
-          <p>Memory Box is an app designed to assist Alzheimer's patients by helping them store, organize, 
-            and recall important memories, made using Flutter and Firebase.</p>
-            <span className='f'>github-link: <a href='https://github.com/vaishnavi185/Memory-Box'>click here </a></span>
+          <h5 className='head'>WEB-DEVELOPMENT</h5>
+          <p>I am a MERN stack developer proficient in HTML, CSS, and JavaScript. I have completed several projects that showcase my skills in creating dynamic and responsive web applications. My expertise includes developing with MongoDB, Express.js, React, and Node.js, enabling me to build full-stack applications. </p>
+           
             </div>
           <div className='Box' style={{background : 'black'}}>
 
           <img src={bgImage1} alt="img" className='im0' />
-          <h5 className='head' style={{color:'#BB86FC'}}>Online-shopping-web</h5>
-          < p style={{color:'#BB86FC'}}>Online Pizza Shopping and Delivery Platform 
-            Developed Using the MERN Stack</p>
-            <span className='f' style={{color:'#BB86FC'}}>github-link: <a href='https://github.com/vaishnavi185/e-commerce'>click here </a></span>
+          <h5 className='head' style={{color:'#BB86FC'}}>APP-DEVELOPMENT</h5>
+          < p style={{color:'#BB86FC'}}>I am a proficient Flutter developer with a focus on app front-end development. I excel in creating intuitive and visually appealing user interfaces using Android Studio. My portfolio includes diverse projects showcasing my ability to build high-performance mobile applications for  Android platforms, leveraging my skills to deliver seamless and engaging user experiences.</p>
+           
             </div>
           <div className='Box'style={{background : 'black'}}>
           <img src={bgImage2} alt="img" className='im0' />
-          <h5 className='head' style={{color:'#03DAC6'}}>Quize-web-app</h5>
-          < p style={{color:'#03DAC6'}}>A simple quiz web app made in JavaScript, HTML, and CSS that calculates scores of correct and incorrect answers.</p>
-            <span className='f' style={{color:'#03DAC6'}}>github-link: <a href='https://github.com/vaishnavi185/quize-web-'>click here </a></span>
+          <h5 className='head' style={{color:'#03DAC6'}}>UI/UX</h5>
+          < p style={{color:'#03DAC6'}}>I am a UI/UX designer skilled in creating visually appealing and user-friendly designs. I use Figma for designing intuitive interfaces and Canva for crafting engaging presentations. My expertise lies in enhancing user experiences through thoughtful design and clear, compelling visuals.</p>
+           
           
           </div>
         </div>
